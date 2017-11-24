6a6e39b26849f17df427943dc8cc6c722260dcb8
Günther Foidl <gue@korporal.at>
2017-11-24 21:04:50 +0100

    Simple Addition

---
+++ source/ExpressionEvaluator/BasicExpressionEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace ExpressionEvaluator
+{
+    public class BasicExpressionEvaluator
+    {
+        static BasicExpressionEvaluator()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        }
+        //---------------------------------------------------------------------
+        public double Evaluate(string expression)
+        {
+            if (expression.Contains("+"))
+            {
+                var parts = expression.Split('+');
+
+                Expression sum = Expression.Add(
+                    Expression.Constant(double.Parse(parts[0])),
+                    Expression.Constant(double.Parse(parts[1]))
+                );
+                var lambda = Expression.Lambda<Func<double>>(sum);
+                var compiled = lambda.Compile();
+                return compiled();
+            }
+
+            double value = 0;
+            double.TryParse(expression, out value);
+
+            return value;
+        }
+    }
+}
---
+++ tests/ExpressionEvaluator.Tests/BasicExpressionEvaluatorTests/Evaluate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ExpressionEvaluator.Tests.BasicExpressionEvaluatorTests
+{
+    [TestFixture]
+    public class Evaluate
+    {
+        private Random _rnd = new Random();
+        //---------------------------------------------------------------------
+        [Test]
+        public void Empty_string___is_0([Values("", " ", "\t")]string value)
+        {
+            var sut = new BasicExpressionEvaluator();
+
+            double actual = sut.Evaluate(value);
+
+            Assert.AreEqual(0, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Double___OK()
+        {
+            double left = _rnd.Next(1, 100);
+            double right = _rnd.Next(1, 100);
+            double input = left + right / 100d;
+
+            var sut = new BasicExpressionEvaluator();
+
+            double actual = sut.Evaluate(input.ToString());
+
+            Assert.AreEqual(input, actual, 1e-10);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Add_two_numbers___OK([Values("2.7+3.2", "2.7 + 3.2")]string expression)
+        {
+            var sut = new BasicExpressionEvaluator();
+
+            double actual = sut.Evaluate(expression);
+
+            Assert.AreEqual(2.7 + 3.2, actual, 1e-10);
+        }
+    }
+}
