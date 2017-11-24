ed41e9e53a0a8a2302125136257b475e821f27d6
Günther Foidl <gue@korporal.at>
2017-11-24 23:34:26 +0100

    multiple variables

--- source/ExpressionEvaluator/BasicExpressionEvaluator.cs
+++ source/ExpressionEvaluator/BasicExpressionEvaluator.cs
@@ -9,22 +9,24 @@ namespace ExpressionEvaluator
 {
     public class BasicExpressionEvaluator
     {
-        private readonly List<ParameterExpression> _parameters      = new List<ParameterExpression>();
-        private readonly Stack<Expression>         _expressionStack = new Stack<Expression>();
-        private readonly Stack<Symbol>             _operationStack  = new Stack<Symbol>();
+        private readonly List<string>      _parameters      = new List<string>();
+        private readonly Stack<Expression> _expressionStack = new Stack<Expression>();
+        private readonly Stack<Symbol>     _operationStack  = new Stack<Symbol>();
         //---------------------------------------------------------------------
         static BasicExpressionEvaluator()
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
         }
         //---------------------------------------------------------------------
-        public double Evaluate(string expression, double variable = 0)
+        public double Evaluate(string expression, params double[] arguments)
         {
             if (string.IsNullOrWhiteSpace(expression)) return 0;
 
             _parameters     .Clear();
             _expressionStack.Clear();
             _operationStack .Clear();
+
+            var arrayParameter = Expression.Parameter(typeof(double[]), "args");
 
             var sr = new StringReader(expression);
             int peek;
@@ -41,8 +43,7 @@ namespace ExpressionEvaluator
 
                 if (char.IsLetter(next))
                 {
-                    ParameterExpression parameterExpression = this.ReadParameter(sr);
-                    _parameters.Add(parameterExpression);
+                    Expression parameterExpression = this.ReadParameter(sr, arrayParameter);
                     _expressionStack.Push(parameterExpression);
                     continue;
                 }
@@ -87,16 +88,8 @@ namespace ExpressionEvaluator
 
             this.EvaluateWhile(() => _operationStack.Count > 0);
 
-            if (_parameters.Count == 0)
-            {
-                var compiled = Expression.Lambda<Func<double>>(_expressionStack.Pop()).Compile();
-                return compiled();
-            }
-            else
-            {
-                var compiled = Expression.Lambda<Func<double, double>>(_expressionStack.Pop(), _parameters).Compile();
-                return compiled(variable);
-            }
+            var compiled = Expression.Lambda<Func<double[], double>>(_expressionStack.Pop(), arrayParameter).Compile();
+            return compiled(arguments);
         }
         //---------------------------------------------------------------------
         private Expression ReadOperand(StringReader sr)
@@ -126,7 +119,7 @@ namespace ExpressionEvaluator
             return (Operation)operation;
         }
         //---------------------------------------------------------------------
-        private ParameterExpression ReadParameter(StringReader sr)
+        private Expression ReadParameter(StringReader sr, Expression arrayParameter)
         {
             var sb = new StringBuilder();
             int peek;
@@ -144,7 +137,12 @@ namespace ExpressionEvaluator
                     break;
             }
 
-            return Expression.Parameter(typeof(double), sb.ToString());
+            string parameter = sb.ToString();
+
+            if (!_parameters.Contains(parameter))
+                _parameters.Add(parameter);
+
+            return Expression.ArrayIndex(arrayParameter, Expression.Constant(_parameters.IndexOf(parameter)));
         }
         //---------------------------------------------------------------------
         private void EvaluateWhile(Func<bool> condition)
--- tests/ExpressionEvaluator.Tests/BasicExpressionEvaluatorTests/Evaluate.cs
+++ tests/ExpressionEvaluator.Tests/BasicExpressionEvaluatorTests/Evaluate.cs
@@ -130,5 +130,33 @@ namespace ExpressionEvaluator.Tests.BasicExpressionEvaluatorTests
 
             Assert.AreEqual(Math.PI, actual, 1e-10);
         }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Variable_and_other_math___OK()
+        {
+            double a          = Math.PI;
+            string expression = "2*a+3-4*a";
+
+            var sut = new BasicExpressionEvaluator();
+
+            double actual = sut.Evaluate(expression, a);
+
+            Assert.AreEqual(2 * a + 3 - 4 * a, actual, 1e-10);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Multiple_variables___OK()
+        {
+            double a          = 2.6;
+            double b          = 5.7;
+            double c          = 24.15;
+            string expression = "(((9-a/2)*2-b)/2-a-1)/(2+c/(2+4))";
+
+            var sut = new BasicExpressionEvaluator();
+
+            double actual = sut.Evaluate(expression, a, b, c);
+
+            Assert.AreEqual((((9 - a / 2) * 2 - b) / 2 - a - 1) / (2 + c / (2 + 4)), actual, 1e-10);
+        }
     }
 }
