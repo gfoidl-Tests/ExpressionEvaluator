9d8afa078a1e4ecf4043e7113fa8b6e73a7b2108
Günther Foidl <gue@korporal.at>
2017-11-28 10:45:31 +0100

    CppCompiler added

---
+++ demos/ExpressionCompiler.Cpp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ExpressionCompiler.Cpp
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Directory.CreateDirectory("cpp");
+            Console.WriteLine();
+
+            Run("(2+3)*5", "simple");
+            Run("sin(x)(2+3)*5+log(a)", "vars");
+        }
+        //---------------------------------------------------------------------
+        private static void Run(string expression, string cppName)
+        {
+            cppName = Path.ChangeExtension(Path.Combine("cpp", cppName), "cpp");
+
+            using (StreamWriter sw = File.CreateText(cppName))
+            {
+                var compiler = new CppCompiler(sw);
+                compiler.Compile(expression);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(File.ReadAllText(cppName));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/ExpressionCompiler/Emitter/Cpp/CppEmitter.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ExpressionCompiler.Expressions;
+
+namespace ExpressionCompiler.Emitter.Cpp
+{
+    internal class CppEmitter : Emitter<bool>
+    {
+        private readonly TextWriter                  _writer;
+        private readonly IReadOnlyCollection<string> _parameters;
+        private bool                                 _isFirstBinary = true;
+        //---------------------------------------------------------------------
+        public CppEmitter(Expression tree, IReadOnlyCollection<string> parameters, TextWriter writer)
+            : base(tree)
+        {
+            _writer     = writer;
+            _parameters = parameters;
+        }
+        //---------------------------------------------------------------------
+        public override void Emit()
+        {
+            _writer.WriteLine("#include <math.h>");
+            _writer.WriteLine("#include <iostream>");
+            _writer.WriteLine();
+            _writer.WriteLine("using namespace std;");
+            _writer.WriteLine();
+            this.EmitArgs();
+            _writer.WriteLine("int main()");
+            _writer.WriteLine("{");
+            _writer.Write("\tdouble res = ");
+
+            this.Tree.Accept(this);
+
+            _writer.WriteLine(";");
+            _writer.WriteLine();
+            _writer.WriteLine("\tcout << res << endl;");
+            _writer.WriteLine("}");
+        }
+        //---------------------------------------------------------------------
+        private void EmitArgs()
+        {
+            if ((_parameters?.Count ?? 0) == 0) return;
+
+            _writer.WriteLine($"double args[{_parameters.Count}]; // supply arguments");
+            _writer.WriteLine();
+        }
+        //---------------------------------------------------------------------
+        public override bool Visit(ConstantExpression constant)
+        {
+            _writer.Write(constant.Value);
+            return true;
+        }
+        //---------------------------------------------------------------------
+        public override bool Visit(ArrayIndexExpression arrayIndexExpression)
+        {
+            _writer.Write("args[");
+
+            arrayIndexExpression.Right.Accept(this);
+
+            _writer.Write("]");
+
+            return true;
+        }
+        //---------------------------------------------------------------------
+        public override bool Visit(AddExpression addExpression)                     => this.VisitBinaryCore(addExpression, "+");
+        public override bool Visit(SubtractExpression subtractExpression)           => this.VisitBinaryCore(subtractExpression, "-");
+        public override bool Visit(MultiplyExpression multiplyExpression)           => this.VisitBinaryCore(multiplyExpression, "*");
+        public override bool Visit(DivideExpression divideExpression)               => this.VisitBinaryCore(divideExpression, "/");
+        public override bool Visit(ExponentationExpression exponentationExpression) => this.VisitBinaryCore(exponentationExpression, "pow");
+        public override bool Visit(ModuloExpression moduloExpression)               => this.VisitBinaryCore(moduloExpression, "%");
+        public override bool Visit(SinExpression sinExpression)                     => this.VisitInstrinsicsCore(sinExpression, "sin");
+        public override bool Visit(CosExpression cosExpression)                     => this.VisitInstrinsicsCore(cosExpression, "cos");
+        public override bool Visit(TanExpression tanExpression)                     => this.VisitInstrinsicsCore(tanExpression, "tan");
+        public override bool Visit(LogExpression logExpression)                     => this.VisitInstrinsicsCore(logExpression, "log");
+        //---------------------------------------------------------------------
+        private bool VisitBinaryCore(BinaryExpression binaryExpression, string cmd)
+        {
+            bool isFirstBinary = _isFirstBinary;
+            _isFirstBinary = false;
+
+            if (!isFirstBinary)
+                _writer.Write("(");
+
+            binaryExpression.Left.Accept(this);
+            _writer.Write($" {cmd} ");
+            binaryExpression.Right.Accept(this);
+
+            if (!isFirstBinary)
+                _writer.Write(")");
+
+            return true;
+        }
+        //---------------------------------------------------------------------
+        private bool VisitInstrinsicsCore(IntrinsicExpression intrinsic, string cmd)
+        {
+            _writer.Write(cmd);
+            _writer.Write("(");
+
+            intrinsic.Argument.Accept(this);
+
+            _writer.Write(")");
+
+            return true;
+        }
+    }
+}
